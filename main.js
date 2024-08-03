54b28bb6c2917ddaadc40cebee7f77cfe18d66a7
Ismail <ismailsajid.kapadia@dcmail.ca>
2024-08-02 21:35:49 -0400

    Updates

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
+
+// Generate a random integer between min and max, inclusive
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Generate a random RGB color string
+function randomRGB() {
+    return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+}
+
